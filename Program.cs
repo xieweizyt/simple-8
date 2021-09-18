43f9c60a97ca547e54220b89f02c63c9c68a289a
1932660418 <1932660418@qq.com>
2021-09-18 23:37:10 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace simple_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region async/await 执行顺序
+            {
+                SimpleAsync.Show();
+            }
+            #endregion
+
+
+            #region async/await 执行顺序
+            {
+
+            }
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
---
+++ SimpleAsync.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace simple_8
+{
+    public static class SimpleAsync
+    {
+        public static void Show()
+        {
+            Console.WriteLine($"statr show thread id = {Thread.CurrentThread.ManagedThreadId.ToString("00")}");
+            ShowTest1();
+            Console.WriteLine($"end show thread id = {Thread.CurrentThread.ManagedThreadId.ToString("00")}");
+        }
+
+        private static async Task ShowTest1()
+        {
+            Console.WriteLine($"statr ShowTest1 thread id = {Thread.CurrentThread.ManagedThreadId.ToString("00")}");
+
+            await Task.Run(() =>
+            {
+                Console.WriteLine($"statr Task thread id = {Thread.CurrentThread.ManagedThreadId.ToString("00")}");
+                Thread.Sleep(1000);
+                Console.WriteLine($"end Task thread id = {Thread.CurrentThread.ManagedThreadId.ToString("00")}");
+            });
+
+            Console.WriteLine($"end ShowTest1 thread id = {Thread.CurrentThread.ManagedThreadId.ToString("00")}");
+        }
+    }
+}
